a32930b524cf0fd938bce98e9b80a51b6964d597
Kbot <nft@tokensplease.com>
2022-03-21 14:48:39 -0700

    update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,10 @@
 const contractAddress = "0x5266B9d8890A348A0717b531Cd1ae60a5c753b8c"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the throwing up tokens!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'The TokensPlease NFT Project is a decentralized, art project that aims to cultivate and curate a generation.  <a href="https://tokensplease.io"  target="_blank" rel="noopener noreferrer">TokensPleaseNFT</a> Discord';
+const h1_presale_coming_soon = "Regurgitating Soon";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
