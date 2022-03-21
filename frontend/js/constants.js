f236d9e0d3152986d0cb10fe98fe15e42262e977
Kbot <nft@tokensplease.com>
2022-03-21 14:48:09 -0700

    update changes

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x5266B9d8890A348A0717b531Cd1ae60a5c753b8c"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
